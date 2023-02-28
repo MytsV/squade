0d384d966766077b8a3f588167291a00cb2e3519
MytsV <mytsvictoria@gmail.com>
2023-03-01 00:02:49 +0200

    Implement quad equation solver. Add tests
    Roots are found via discriminant formula from high school
    math. The basic function provides results in a form of an array.
    
    Implement tests in basic human-readable format. The results of
    calculations are appended to stdout.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+const getDiscriminant = (a, b, c) => {
+  return b * b - 4 * a * c;
+};
+
+const getSolution = (a, b, c) => {
+  const discriminant = getDiscriminant(a, b, c);
+  if (discriminant > 0) {
+    return [
+      (- b + Math.sqrt(discriminant)) / (2 * a),
+      (- b - Math.sqrt(discriminant)) / (2 * a),
+    ];
+  } else if (discriminant === 0) {
+    return [
+      - b / (2 * a),
+    ];
+  } else {
+    return [];
+  }
+};
+
+// Two different roots
+console.log(getSolution(1, -26, 120));
+// One root
+console.log(getSolution(9, -12, 4));
+// No real roots
+console.log(getSolution(9, 1, 4));
