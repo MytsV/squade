43a384b2c7ae2b7adebe2cf9011d5463726363f8
MytsV <mytsvictoria@gmail.com>
2023-03-01 00:17:50 +0200

    Actually (!) improve naming and math operations notation
    Bring more clarity to the code with better function names.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ const getDiscriminant = (a, b, c) => {
   return b * b - 4 * a * c;
 };
 
-const getSolution = (a, b, c) => {
+const getRoots = (a, b, c) => {
   const discriminant = getDiscriminant(a, b, c);
   if (discriminant > 0) {
     return [
-      (- b + Math.sqrt(discriminant)) / (2 * a),
-      (- b - Math.sqrt(discriminant)) / (2 * a),
+      (- b + Math.sqrt(discriminant)) / 2 / a,
+      (- b - Math.sqrt(discriminant)) / 2 / a,
     ];
   } else if (discriminant === 0) {
     return [
-      - b / (2 * a),
+      - b / 2 / a,
     ];
   } else {
     return [];
@@ -19,8 +19,8 @@ const getSolution = (a, b, c) => {
 };
 
 // Two different roots
-console.log(getSolution(1, -26, 120));
+console.log(getRoots(1, -26, 120));
 // One root
-console.log(getSolution(9, -12, 4));
+console.log(getRoots(9, -12, 4));
 // No real roots
-console.log(getSolution(9, 1, 4));
+console.log(getRoots(9, 1, 4));
