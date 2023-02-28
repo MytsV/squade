b447a7ce7aa5d15afee024484c56031c3d86f746
MytsV <mytsvictoria@gmail.com>
2023-03-01 00:12:49 +0200

    Revert "Fix naming"
    This reverts commit aa2ebf4b79ed7d51b39633de46916ba8fa8bd1b7.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const getSexist = (a, b, c) => {
+const getDiscriminant = (a, b, c) => {
   return b * b - 4 * a * c;
 };
 
-const roots = (FIRST_COEF, b, c) => {
-  const D = getSexist(FIRST_COEF, b, c);
-  if (D > 0) {
+const getSolution = (a, b, c) => {
+  const discriminant = getDiscriminant(a, b, c);
+  if (discriminant > 0) {
     return [
-      (- b + Math.sqrt(D)) / (2 * FIRST_COEF),
-      (- b - Math.sqrt(D)) / (2 * FIRST_COEF),
+      (- b + Math.sqrt(discriminant)) / (2 * a),
+      (- b - Math.sqrt(discriminant)) / (2 * a),
     ];
-  } else if (D === 0) {
+  } else if (discriminant === 0) {
     return [
-      - b / (2 * FIRST_COEF),
+      - b / (2 * a),
     ];
   } else {
     return [];
@@ -19,8 +19,8 @@ const roots = (FIRST_COEF, b, c) => {
 };
 
 // Two different roots
-console.log(roots(1, -26, 120));
+console.log(getSolution(1, -26, 120));
 // One root
-console.log(roots(9, -12, 4));
+console.log(getSolution(9, -12, 4));
 // No real roots
-console.log(roots(9, 1, 4));
+console.log(getSolution(9, 1, 4));
