8fff73a3c12fb9e26cedc15dced96415582a566d
MytsV <mytsvictoria@gmail.com>
2023-03-01 02:01:59 +0200

    Implement interactive query retrieval
    Use readline module to create an easy-readable stdin communication.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const readline = require('readline');
+const {stdin: input, stdout: output} = process;
+
 const getDiscriminant = (a, b, c) => {
   return b * b - 4 * a * c;
 };
@@ -20,5 +23,47 @@ const getRoots = (a, b, c) => {
     return [];
   }
 };
+
+const isNumber = (n) => {
+  return !isNaN(n) && isFinite(n);
+};
+
+const getQueryInteractive = async () => {
+  const query = [];
+  const rl = readline.createInterface({input, output});
+  const questionNumber = (prompt) => {
+    return new Promise((resolve) => {
+      rl.question(prompt, async (answer) => {
+        const answerParsed = parseFloat(answer);
+        if (!isNumber(answerParsed)) {
+          console.error(
+              `Error: expected a valid number, got ${answer} instead`,
+          );
+          await questionNumber(prompt);
+        } else {
+          query.push(answerParsed);
+        }
+        resolve();
+      });
+    });
+  };
+  await questionNumber('a = ');
+  await questionNumber('b = ');
+  await questionNumber('c = ');
+  rl.close();
+  return query;
+};
+
+const solve = async () => {
+  let query;
+  if (process.argv.length > 2) {
+    throw Error('Not implemented yet!');
+  } else {
+    query = await getQueryInteractive();
+  }
+  console.log(query);
+};
+
+solve();
 
 module.exports = {getRoots};
