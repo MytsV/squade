03bcfdad9644e3f6fb5117e4c09a476996f45c29
MytsV <mytsvictoria@gmail.com>
2023-03-06 17:54:21 +0200

    Refactor interactive.js
    Decompose a big function.

--- src/interactive.js
+++ src/interactive.js
@@ -5,24 +5,26 @@ const isNumeric = (n) => {
   return /^-?\d+\.?\d*$/.test(n);
 };
 
+const questionnaire = ({rl, query}) => (prompt) => {
+  return new Promise((resolve) => {
+    rl.question(prompt, async (answer) => {
+      if (!isNumeric(answer)) {
+        console.error(
+          `Error: expected a valid number, got ${answer} instead`,
+        );
+        await questionnaire({rl, query})(prompt);
+      } else {
+        query.push(Number(answer));
+      }
+      resolve();
+    });
+  });
+};
+
 const getQueryInteractive = async () => {
   const query = [];
   const rl = readline.createInterface({input, output});
-  const questionNumber = (prompt) => {
-    return new Promise((resolve) => {
-      rl.question(prompt, async (answer) => {
-        if (!isNumeric(answer)) {
-          console.error(
-            `Error: expected a valid number, got ${answer} instead`,
-          );
-          await questionNumber(prompt);
-        } else {
-          query.push(Number(answer));
-        }
-        resolve();
-      });
-    });
-  };
+  const questionNumber = questionnaire({rl, query});
   await questionNumber('a = ');
   await questionNumber('b = ');
   await questionNumber('c = ');
